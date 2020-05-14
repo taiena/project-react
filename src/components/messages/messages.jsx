3ffeba3f1150445bc7d8c37725299f71373ae765
Anna Domaskina <taienametztli@mail.ru>
2020-05-14 21:29:50 +0300

    added components to messages page

--- src/components/messages/messages.jsx
+++ src/components/messages/messages.jsx
@@ -2,20 +2,34 @@ import React from "react";
 import classes from "./messages.module.scss";
 import { NavLink } from "react-router-dom";
 
+const Dialog = (props) => {
+  let path = "/messages/" + props.id;
+
+  return (
+    <div className={classes.dialog + " " + classes.active}>
+      <NavLink to={path}>{props.name}</NavLink>
+    </div>
+  );
+};
+
+const Message = (props) => {
+  return (
+    <div className={classes.message}>
+      <div>{props.message}</div>
+    </div>
+  );
+};
+
 const Messages = () => {
   return (
     <div className={classes.messages}>
       <div className={classes.dialogList}>
-        <div className={classes.dialog + " " + classes.active}>
-          <NavLink to="/messages/1">Lena</NavLink>
-        </div>
-        <div className={classes.dialog}>
-          <NavLink to="/messages/2">Vova</NavLink>
-        </div>
+        <Dialog name="Lena" id="1" />
+        <Dialog name="Vova" id="2" />
       </div>
       <div className={classes.messageList}>
-        <div className={classes.message}>Hi</div>
-        <div className={classes.message}>Hello</div>
+        <Message message="Hi" />
+        <Message message="Hello" />
       </div>
     </div>
   );
