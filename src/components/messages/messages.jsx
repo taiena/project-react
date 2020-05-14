130760d56b9a0c6e1d3e8633790771a961242921
Anna Domaskina <taienametztli@mail.ru>
2020-05-14 20:27:04 +0300

    started messages page, changes some common styles

--- src/components/messages/messages.jsx
+++ src/components/messages/messages.jsx
@@ -1,10 +1,22 @@
 import React from "react";
 import classes from "./messages.module.scss";
+import { NavLink } from "react-router-dom";
 
 const Messages = () => {
   return (
     <div className={classes.messages}>
-      <span>Messages page</span>
+      <div className={classes.dialogList}>
+        <div className={classes.dialog + " " + classes.active}>
+          <NavLink to="/messages/1">Lena</NavLink>
+        </div>
+        <div className={classes.dialog}>
+          <NavLink to="/messages/2">Vova</NavLink>
+        </div>
+      </div>
+      <div className={classes.messageList}>
+        <div className={classes.message}>Hi</div>
+        <div className={classes.message}>Hello</div>
+      </div>
     </div>
   );
 };
