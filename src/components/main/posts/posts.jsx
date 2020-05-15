e4b26c9f4aed134197be98d5f51c530c3d63c873
Anna Domaskina <taienametztli@mail.ru>
2020-05-15 17:56:06 +0300

    refactoring posts and messages pages, add .map

--- src/components/main/posts/posts.jsx
+++ src/components/main/posts/posts.jsx
@@ -2,17 +2,37 @@ import React from "react";
 import classes from "./posts.module.scss";
 import Post from "./post/post.jsx";
 
-const Posts = () => {
+const Posts = (props) => {
+  let posts = [
+    { id: 1, message: "Hi, how are you?", likesCount: 3 },
+    { id: 2, message: "It is so amazing!", likesCount: 12 },
+    { id: 3, message: "It is my first post", likesCount: 5 },
+  ];
+
+  let postsElements = posts.map((p) => (
+    <Post message={p.message} likesCount={p.likesCount} />
+  ));
+
   return (
     <div className={classes.posts}>
       <div className={classes.inputpost}>
         <textarea></textarea>
         <button>Add post</button>
       </div>
-      <Post message="Hi, how are you?" likesCount="1" />
-      <Post message="It is my first post" likesCount="5" />
+      {postsElements}
     </div>
   );
 };
 
 export default Posts;
+
+// пример без массива, с передачей пропсов
+
+// const Posts = (props) => {
+//   return (
+//     <div className={classes.posts}>
+//       <Post message="Hi, how are you?" likesCount="1" />
+//       <Post message="It is my first post" likesCount="5" />
+//     </div>
+//   );
+// };
--- src/components/messages/messages.jsx
+++ src/components/messages/messages.jsx
@@ -21,18 +21,40 @@ const Message = (props) => {
 };
 
 const Messages = () => {
+  let dialogs = [
+    { id: 1, name: "Lena" },
+    { id: 2, name: "Vova" },
+  ];
+
+  let messages = [
+    { id: 1, message: "Hi" },
+    { id: 2, message: "Hello" },
+  ];
+
+  let dialogsElements = dialogs.map((d) => <Dialog name={d.name} id={d.id} />);
+
+  let messagesElements = messages.map((m) => <Message message={m.message} />);
+
   return (
     <div className={classes.messages}>
-      <div className={classes.dialogList}>
-        <Dialog name="Lena" id="1" />
-        <Dialog name="Vova" id="2" />
-      </div>
-      <div className={classes.messageList}>
-        <Message message="Hi" />
-        <Message message="Hello" />
-      </div>
+      <div className={classes.dialogList}>{dialogsElements}</div>
+      <div className={classes.messageList}>{messagesElements}</div>
     </div>
   );
 };
 
 export default Messages;
+
+// пример с массивом dialogs, но без map
+
+// const Messages = () => {
+
+//   return (
+//     <div className={classes.messages}>
+//       <div className={classes.dialogList}>
+//         <Dialog name={dialogs[0].name} id={dialogs[0].id} />
+//         <Dialog name={dialogs[1].name} id={dialogs[1].id} />
+//       </div>
+//     </div>
+//   );
+// };
