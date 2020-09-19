a2380de945af8c4c637e464d08d66f660bfcf650
Anna Domaskina <taienametztli@mail.ru>
2020-09-19 16:09:01 +0300

    refactoring mainReducer

--- src/redux/mainReducer.js
+++ src/redux/mainReducer.js
@@ -7,27 +7,32 @@ let initialState = {
     { id: 2, message: "It is so cool!", likesCount: 12 },
     { id: 3, message: "It is my first post", likesCount: 5 },
   ],
-  newPostText: "Enter your post here",
+  newPostText: "Enter your post here", // value from textarea
 };
 
 const mainReducer = (state = initialState, action) => {
   switch (action.type) {
+    // добавление нового поста на стену
     case ADD_POST: {
       let newPost = {
         id: 5,
         message: state.newPostText,
         likesCount: 0,
       };
-      let stateCopy = { ...state };
-      stateCopy.posts = [...state.posts];
-      stateCopy.posts.push(newPost);
-      stateCopy.newPostText = "";
-      return stateCopy;
+      return {
+        ...state, // возвращаем поверхностную копию стейта
+        // возвращаем копию массива posts и добавляем в конце новый элемент
+        posts: [...state.posts, newPost],
+        newPostText: "", // обнуляем textarea
+      };
     }
+
+    // добавление в стейт нового впечатанного символа
     case UPDATE_NEW_POST_TEXT: {
-      let stateCopy = { ...state };
-      stateCopy.newPostText = action.newText;
-      return stateCopy;
+      return {
+        ...state, // возвращаем поверхностную копию стейта
+        newPostText: action.newText, // добавляем впечатанный символ из textarea
+      };
     }
     default:
       return state;
