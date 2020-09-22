8b4f2a9ff26063ca66b2c70e1615215781b5d7ce
Anna Domaskina <taienametztli@mail.ru>
2020-09-22 14:27:20 +0300

    refactoring Users to class component

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,65 +1,64 @@
-import React from "react";
+import React, { Component } from "react";
 import classes from "./Users.module.scss";
 import * as axios from "axios";
 import userAva from "../../assets/images/ava.png";
 
-let Users = (props) => {
-  let getUsers = () => {
+export default class Users extends Component {
+  componentDidMount() {
     // изначально юзеров в стейте нет, если их нет, то задиспатчится setUsers
     // юзеры запишутся в стейт и компонент перерисуется с ними
-    if (props.users.length === 0) {
+    if (this.props.users.length === 0) {
       // после получения api приходит response, в нем сидит data, в ней массив items
       axios
         .get("https://social-network.samuraijs.com/api/1.0/users")
         .then((response) => {
-          props.setUsers(response.data.items);
+          this.props.setUsers(response.data.items);
         });
     }
-  };
+  }
 
-  return (
-    <div className={classes.Users}>
-      <button onClick={getUsers}>Get users</button>
-      {props.users.map((user) => (
-        <div className={classes.User} key={user.id}>
-          <div className={classes.UserAva}>
-            <img
-              src={user.photos.small != null ? user.photos.small : userAva}
-            />
-          </div>
-          <div className={classes.UserBtn}>
-            {user.followed ? (
-              <button
-                onClick={() => {
-                  props.unfollow(user.id);
-                }}
-              >
-                Unollow
-              </button>
-            ) : (
-              <button
-                onClick={() => {
-                  props.follow(user.id);
-                }}
-              >
-                Follow
-              </button>
-            )}
-          </div>
-          <div className={classes.UserInfo}>
-            <div className={classes.UserPerson}>
-              <div>{user.name}</div>
-              <div>{user.status}</div>
+  render() {
+    return (
+      <div className={classes.Users}>
+        {this.props.users.map((user) => (
+          <div className={classes.User} key={user.id}>
+            <div className={classes.UserAva}>
+              <img
+                src={user.photos.small != null ? user.photos.small : userAva}
+              />
             </div>
-            <div className={classes.UserLocation}>
-              <div>{"user.location.country"}</div>
-              <div>{"user.location.city"}</div>
+            <div className={classes.UserBtn}>
+              {user.followed ? (
+                <button
+                  onClick={() => {
+                    this.props.unfollow(user.id);
+                  }}
+                >
+                  Unollow
+                </button>
+              ) : (
+                <button
+                  onClick={() => {
+                    this.props.follow(user.id);
+                  }}
+                >
+                  Follow
+                </button>
+              )}
+            </div>
+            <div className={classes.UserInfo}>
+              <div className={classes.UserPerson}>
+                <div>{user.name}</div>
+                <div>{"user.status"}</div>
+              </div>
+              <div className={classes.UserLocation}>
+                <div>{"user.location.country"}</div>
+                <div>{"user.location.city"}</div>
+              </div>
             </div>
           </div>
-        </div>
-      ))}
-    </div>
-  );
-};
-
-export default Users;
+        ))}
+      </div>
+    );
+  }
+}
