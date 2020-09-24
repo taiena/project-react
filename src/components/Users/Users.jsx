396c64ae580fb1a72b138800ec5768854354de6c
Anna Domaskina <taienametztli@mail.ru>
2020-09-24 13:56:35 +0300

    Users refactored to func, api calls moved to UsersContainer

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,103 +1,74 @@
-import React, { Component } from "react";
+import React from "react";
 import classes from "./Users.module.scss";
-import * as axios from "axios";
 import userAva from "../../assets/images/ava.png";
 
-export default class Users extends Component {
-  componentDidMount() {
-    // после получения api приходит response, в нем сидит data, в ней массив items
-    axios
-      .get(
-        `https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`
-      )
-      .then((response) => {
-        this.props.setUsers(response.data.items);
-        this.props.setTotalUsersCount(response.data.totalCount);
-      });
+let Users = (props) => {
+  // подсчет кол-ва страниц (всех юзеров / кол-во юзеров на странице)
+  let pagesCount = Math.ceil(props.totalUsersCount / props.pageSize);
+  let pages = [];
+  for (let i = 1; i <= pagesCount; i++) {
+    pages.push(i);
   }
 
-  // при клике на номер страницы новый запрос апи с pageNumber
-  onPageChanged = (pageNumber) => {
-    this.props.setCurrentPage(pageNumber);
-    axios
-      .get(
-        `https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`
-      )
-      .then((response) => {
-        this.props.setUsers(response.data.items);
-      });
-  };
+  return (
+    <div className={classes.Users}>
+      {/* вывод массива нормеров страниц */}
+      <div>
+        {pages.map((page) => {
+          return (
+            <span
+              className={props.currentPage === page && classes.selectedPage}
+              onClick={(e) => {
+                props.onPageChanged(page);
+              }}
+            >
+              {page}&nbsp;
+            </span>
+          );
+        })}
+      </div>
 
-  render() {
-    // подсчет кол-ва страниц (всех юзеров / кол-во юзеров на странице)
-    let pagesCount = Math.ceil(
-      this.props.totalUsersCount / this.props.pageSize
-    );
-    let pages = [];
-    for (let i = 1; i <= pagesCount; i++) {
-      pages.push(i);
-    }
-
-    return (
-      <div className={classes.Users}>
-        {/* вывод массива нормеров страниц */}
-        <div>
-          {pages.map((page) => {
-            return (
-              <span
-                className={
-                  this.props.currentPage === page && classes.selectedPage
-                }
-                onClick={(e) => {
-                  this.onPageChanged(page);
+      {/* вывод массива users */}
+      {props.users.map((user) => (
+        <div className={classes.User} key={user.id}>
+          <div className={classes.UserAva}>
+            <img
+              src={user.photos.small != null ? user.photos.small : userAva}
+            />
+          </div>
+          <div className={classes.UserBtn}>
+            {user.followed ? (
+              <button
+                onClick={() => {
+                  props.unfollow(user.id);
                 }}
               >
-                {page}&nbsp;
-              </span>
-            );
-          })}
-        </div>
-
-        {/* вывод массива users */}
-        {this.props.users.map((user) => (
-          <div className={classes.User} key={user.id}>
-            <div className={classes.UserAva}>
-              <img
-                src={user.photos.small != null ? user.photos.small : userAva}
-              />
+                Unollow
+              </button>
+            ) : (
+              <button
+                onClick={() => {
+                  props.follow(user.id);
+                }}
+              >
+                Follow
+              </button>
+            )}
+          </div>
+          <div className={classes.UserInfo}>
+            <div className={classes.UserPerson}>
+              <div>{user.name}</div>
+              <div>{user.status}</div>
             </div>
-            <div className={classes.UserBtn}>
-              {user.followed ? (
-                <button
-                  onClick={() => {
-                    this.props.unfollow(user.id);
-                  }}
-                >
-                  Unollow
-                </button>
-              ) : (
-                <button
-                  onClick={() => {
-                    this.props.follow(user.id);
-                  }}
-                >
-                  Follow
-                </button>
-              )}
-            </div>
-            <div className={classes.UserInfo}>
-              <div className={classes.UserPerson}>
-                <div>{user.name}</div>
-                <div>{user.status}</div>
-              </div>
-              <div className={classes.UserLocation}>
-                <div>{"user.location.country"}</div>
-                <div>{"user.location.city"}</div>
-              </div>
+            <div className={classes.UserLocation}>
+              <div>{"user.location.country"}</div>
+              <div>{"user.location.city"}</div>
             </div>
           </div>
-        ))}
-      </div>
-    );
-  }
-}
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Users;
--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { Component } from "react";
+import * as axios from "axios";
 import Users from "./Users.jsx";
 import {
   followAC,
@@ -9,6 +10,46 @@ import {
 } from "../../redux/usersReducer.js";
 
 import { connect } from "react-redux";
+
+class UsersContainer extends Component {
+  componentDidMount() {
+    // после получения api приходит response, в нем сидит data, в ней массив items
+    axios
+      .get(
+        `https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`
+      )
+      .then((response) => {
+        this.props.setUsers(response.data.items);
+        this.props.setTotalUsersCount(response.data.totalCount);
+      });
+  }
+
+  // при клике на номер страницы новый запрос апи с pageNumber
+  onPageChanged = (pageNumber) => {
+    this.props.setCurrentPage(pageNumber);
+    axios
+      .get(
+        `https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`
+      )
+      .then((response) => {
+        this.props.setUsers(response.data.items);
+      });
+  };
+
+  render() {
+    return (
+      <Users
+        totalUsersCount={this.props.totalUsersCount}
+        pageSize={this.props.pageSize}
+        currentPage={this.props.currentPage}
+        onPageChanged={this.onPageChanged}
+        users={this.props.users}
+        follow={this.props.follow}
+        unfollow={this.props.unfollow}
+      />
+    );
+  }
+}
 
 // в компоненту Users придут в пропсах данные из стейта
 let mapStateToProps = (state) => {
@@ -48,6 +89,4 @@ let mapDispatchToProps = (dispatch) => {
 // каждый раз при изменениях в стейте запускается mapStateToProps
 // и формируется новый объект
 
-const UsersContainer = connect(mapStateToProps, mapDispatchToProps)(Users);
-
-export default UsersContainer;
+export default connect(mapStateToProps, mapDispatchToProps)(UsersContainer);
