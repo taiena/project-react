f764845f3b272cc6cd2dfe390745685890a90db2
Anna Domaskina <taienametztli@mail.ru>
2020-10-06 16:17:23 +0300

    del profile header

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -2,12 +2,10 @@ import React from "react";
 import classes from "./Profile.module.scss";
 import PostsContainer from "./Posts/PostsContainer.jsx";
 import ProfileInfo from "./ProfileInfo/ProfileInfo.jsx";
-import ProfileHeader from "./ProfileHeader/ProfileHeader.jsx";
 
 const Profile = (props) => {
   return (
     <main className={classes.ProfilePage}>
-      <ProfileHeader />
       <ProfileInfo profile={props.profile} />
       <PostsContainer />
     </main>
