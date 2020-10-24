679d38df1206a60bdfa5c07365aa949a5885c5c4
Anna Domaskina <taienametztli@mail.ru>
2020-10-24 15:36:00 +0300

    refactoring Users: add User component

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,8 +1,7 @@
 import React from "react";
 import classes from "./Users.module.scss";
-import userAva from "../../assets/images/ava.png";
-import { NavLink } from "react-router-dom";
 import Paginator from "../common/Paginator/Paginator";
+import User from "./User";
 
 let Users = ({
   currentPage,
@@ -23,51 +22,14 @@ let Users = ({
         pageSize={pageSize}
       />
 
-      {/* вывод массива users */}
       {users.map((user) => (
-        <div className={classes.User} key={user.id}>
-          <div className={classes.UserAva}>
-            <NavLink to={"/profile/" + user.id}>
-              <img
-                src={user.photos.small != null ? user.photos.small : userAva}
-                alt=""
-              />
-            </NavLink>
-          </div>
-          <div className={classes.UserBtn}>
-            {user.followed ? (
-              <button
-                // если в массиве хоть одна айди = айди пользователя, тогда button disabled
-                disabled={followingInProgress.some((id) => id === user.id)}
-                onClick={() => {
-                  unfollow(user.id);
-                }}
-              >
-                Unfollow
-              </button>
-            ) : (
-              <button
-                // если в массиве хоть одна айди = айди пользователя, тогда button disabled
-                disabled={followingInProgress.some((id) => id === user.id)}
-                onClick={() => {
-                  follow(user.id);
-                }}
-              >
-                Follow
-              </button>
-            )}
-          </div>
-          <div className={classes.UserInfo}>
-            <div className={classes.UserPerson}>
-              <div>{user.name}</div>
-              <div>{user.status}</div>
-            </div>
-            <div className={classes.UserLocation}>
-              <div>{"user.location.country"}</div>
-              <div>{"user.location.city"}</div>
-            </div>
-          </div>
-        </div>
+        <User
+          key={user.id}
+          user={user}
+          follow={follow}
+          unfollow={unfollow}
+          followingInProgress={followingInProgress}
+        />
       ))}
     </div>
   );
