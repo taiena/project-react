9e594bec97eab9a796f64f15be26023cb65455d5
Anna Domaskina <taienametztli@mail.ru>
2020-10-26 22:55:32 +0300

    add classnames, add buttons prev/next and portions pages to paginator

--- src/components/common/Paginator/Paginator.jsx
+++ src/components/common/Paginator/Paginator.jsx
@@ -1,29 +1,81 @@
-import React from "react";
+import React, { useState } from "react";
 import classes from "./Paginator.module.scss";
+import cn from "classnames";
 
-let Paginator = ({ totalUsersCount, pageSize, currentPage, onPageChanged }) => {
+let Paginator = ({
+  totalItemsCount,
+  pageSize,
+  currentPage,
+  onPageChanged,
+  portionSize = 20,
+}) => {
   // подсчет кол-ва страниц (всех юзеров / кол-во юзеров на странице)
-  let pagesCount = Math.ceil(totalUsersCount / pageSize);
+  let pagesCount = Math.ceil(totalItemsCount / pageSize);
   let pages = [];
   for (let i = 1; i <= pagesCount; i++) {
     pages.push(i);
   }
 
+  // подсчет порций (кол-ва номеров страниц, выводимых за раз)
+  let portionCount = Math.ceil(pagesCount / portionSize);
+  // добавление в стейт номера порции
+  // (чтоб при перезагрузке выводилась та порция, в которой номер текущей страницы)
+  let [portionNumber, setPortionNumber] = useState(
+    Math.ceil(currentPage / portionSize)
+  );
+  // крайний левый номер страницы в порции
+  let leftPortionPageNumber = (portionNumber - 1) * portionSize + 1;
+  // крайний правый номер страницы в порции
+  let rightPortionPageNumber = portionNumber * portionSize;
+
   return (
-    <div>
-      {pages.map((page, index) => {
-        return (
-          <span
-            key={index}
-            className={currentPage === page && classes.selectedPage}
-            onClick={(e) => {
-              onPageChanged(page);
-            }}
-          >
-            {page}&nbsp;
-          </span>
-        );
-      })}
+    <div className={classes.Paginator}>
+      {/* если номер порции больше 1, вывод кнопки prev */}
+      {portionNumber > 1 && (
+        <button
+          onClick={() => {
+            setPortionNumber(portionNumber - 1);
+          }}
+        >
+          PREV
+        </button>
+      )}
+
+      {/* вывод массива страниц с фильтром на текущую порцию */}
+      {pages
+        .filter(
+          (page) =>
+            page >= leftPortionPageNumber && page <= rightPortionPageNumber
+        )
+        .map((page) => {
+          return (
+            <span
+              key={page}
+              className={cn(
+                {
+                  [classes.selectedPage]: currentPage === page,
+                },
+                classes.pageNumber
+              )}
+              onClick={(e) => {
+                onPageChanged(page);
+              }}
+            >
+              {page}
+            </span>
+          );
+        })}
+
+      {/* если порций больше чем текущая порция, вывод кнопки next */}
+      {portionCount > portionNumber && (
+        <button
+          onClick={() => {
+            setPortionNumber(portionNumber + 1);
+          }}
+        >
+          NEXT
+        </button>
+      )}
     </div>
   );
 };
