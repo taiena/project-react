1c9c7bf0107556f5d7cd1c7ba6de35ee6b2e3ccc
Anna Domaskina <taienametztli@mail.ru>
2020-10-26 18:56:45 +0300

    add react-test-renderer, add tests to ProfileStatus

---
+++ src/components/Profile/ProfileInfo/ProfileStatus.test.js
@@ -0,0 +1,53 @@
+import React from "react";
+import { create } from "react-test-renderer";
+import ProfileStatus from "./ProfileStatus";
+
+describe("ProfileStatus component", () => {
+  test("status from props should be in the state", () => {
+    const component = create(<ProfileStatus status="test status" />);
+    const instance = component.getInstance();
+    expect(instance.state.status).toBe("test status");
+  });
+
+  test("after creation <span> with status should be displayed", () => {
+    const component = create(<ProfileStatus status="test status" />);
+    const root = component.root;
+    let span = root.findByType("span");
+    expect(span).not.toBeNull();
+  });
+
+  test("after creation <input> shouldn't be displayed", () => {
+    const component = create(<ProfileStatus status="test status" />);
+    const root = component.root;
+    expect(() => {
+      let input = root.findByType("input");
+    }).toThrow();
+  });
+
+  test("after creation <span> should contains correct status", () => {
+    const component = create(<ProfileStatus status="test status" />);
+    const root = component.root;
+    let span = root.findByType("span");
+    expect(span.children[0]).toBe("test status");
+  });
+
+  test("input should be displayed in editMode instead of span", () => {
+    const component = create(<ProfileStatus status="test status" />);
+    const root = component.root;
+    let span = root.findByType("span");
+    span.props.onDoubleClick();
+    let input = root.findByType("input");
+    expect(input.props.value).toBe("test status");
+  });
+
+  // создаем фейковую функцию, она должна быть вызвана при выходе из edit mode
+  test("callback should be called", () => {
+    const mockCallback = jest.fn();
+    const component = create(
+      <ProfileStatus status="test status" updateUserStatus={mockCallback} />
+    );
+    const instance = component.getInstance();
+    instance.deactivateEditMode();
+    expect(mockCallback.mock.calls.length).toBe(1);
+  });
+});
